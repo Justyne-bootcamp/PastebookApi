fc78e5105c6a30db30fc3750de97fa2b21305d09
mikkodacasinnn <mikko.dacasin@pointwest.com.ph>
2022-05-10 17:54:07 +0800

    username

--- Pastebook.Data/Models/UserAccount.cs
+++ Pastebook.Data/Models/UserAccount.cs
@@ -17,6 +17,7 @@ namespace Pastebook.Data.Models
         }
 
         public Guid UserAccountId { get; set; }
+        public string UserName { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Email { get; set; }
