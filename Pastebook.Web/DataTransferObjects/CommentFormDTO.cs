5d36fc3f71f4de1399e7cf95ae48239a4aa36d14
Justyne-bootcamp <justyne.namay@pointwest.com.ph>
2022-05-13 08:19:57 +0800

    Create commentFormDto

---
+++ Pastebook.Web/DataTransferObjects/CommentFormDTO.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Pastebook.Web.DataTransferObjects
+{
+    public class CommentFormDTO
+    {
+        public Guid PostId { get; set; }
+        public string CommentContent { get; set; }
+    }
+}
